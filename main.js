8ab2b966c844bd353a035083122758bcaa5008cf
Bianca Haux <bhau119@UoA.auckland.ac.nz>
2018-06-15 10:19:59 +1200

    Add JS to cards

--- main.js
+++ main.js
@@ -15,36 +15,42 @@ $("#login").submit(function(e) {
     success: function(data, status, xhr) {
       console.log(data, status, xhr);
 	  
+	  $(".docker-select").show();
+	  $("#login").hide();
+	  
       $("#status").text("Nectar instance ready! Launching container...");
       console.log("launching container on " + data);
-      var image = "jupyter/datascience-notebook";
-      $.ajax("http://" + data + ":8080", {
-        type: "POST",
-        data: {
-          image: image
-        },
-        success: function(data, status, xhr) {
-          var time = 3;
-          setInterval(function(){
-            $("#status").text("Container ready! Redirecting you in " + time);
-            if (time == 0) {
-              window.location.href = data;
-            }
-            time--;
-          }, 1000);
-        },
-        error: function() {
-          $("#status").text("Unable to launch " + image + " - is it accessible on Docker Hub?");
-        }
-      });
+
     },
     error: function() {
       $("#status").text("Login failed - check password?");
       $("#status").attr("class", "alert alert-danger");
-	  
-	  location.replace("./docker.html");
     }
   });
+});
+
+$( ".btn-launch" ).click(function() {
+	
+	var image = $(this)[0].id;
+	$.ajax("http://" + data + ":8080", {
+	type: "POST",
+	data: {
+	  image: image
+	},
+	success: function(data, status, xhr) {
+	  var time = 3;
+	  setInterval(function(){
+		$("#status").text("Container ready! Redirecting you in " + time);
+		if (time == 0) {
+		  window.location.href = data;
+		}
+		time--;
+	  }, 1000);
+	},
+	error: function() {
+	  $("#status").text("Unable to launch " + image + " - is it accessible on Docker Hub?");
+	}
+    });
 });
 
 var svg = d3.select("#bg");
