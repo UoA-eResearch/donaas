8ed00279cf2727357895071f9c2a3d21103f4285
Nick Young <nick.young@auckland.ac.nz>
2018-06-14 15:58:43 +1200

    fancy circles background

--- main.js
+++ main.js
@@ -43,3 +43,21 @@ $("#login").submit(function(e) {
     }
   });
 });
+
+var svg = d3.select("#bg");
+
+d3.interval(function () {
+  var x = Math.random() * parseInt(svg.style('width'));
+  var y = Math.random() * parseInt(svg.style('height'));
+  //console.log("Spawning circle at " + x + "," + y);
+  svg.append('circle')
+    .attr('cx', x)
+    .attr('cy', y)
+    .attr('r', 0)
+    .style("opacity", .2)
+    .transition()
+    .duration(10000)
+    .style("opacity", 0)
+    .attr('r', 500)
+    .on("end", function() { this.remove() })
+}, 1000);
